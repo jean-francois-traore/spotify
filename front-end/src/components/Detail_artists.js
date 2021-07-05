217fc6c84664298ab788a95eb86a89c1d35ac460
inconnu060217 <traore.jeanfrancois1998@gmail.com>
2021-07-05 22:07:31 +0200

    [ADD]stylizations the detail page of the artist and the redirection of the artist's album to the detail page of the album and these tracks

--- front-end/src/components/Detail_artists.js
+++ front-end/src/components/Detail_artists.js
@@ -1,46 +1,66 @@
 import React, { useState, useEffect } from 'react'
-import { useLocation } from 'react-router-dom';
+import { useLocation, useHistory } from 'react-router-dom';
 function Detail_artists() {
     const [detailArtists, setDetailArtists] = useState(null);
     const [albumArtists, setAlbumArtists] = useState(null);
     let location = useLocation();
+    let history = useHistory();
     let name = location.pathname.split("/")[2];
     useEffect(() => {
-        if(!albumArtists){
+        if(!detailArtists){
+            fetch("http://127.0.0.1:5000/artists/" + name)
+            .then(res => res.json())
+            .then(res => setDetailArtists(res))
+        }
+        else if(!albumArtists){
             fetch("http://127.0.0.1:5000/albums/artists/" + name)
             .then(res => res.json())
             .then(res => setAlbumArtists(res))
         }
-    }, [albumArtists])
-    if(!albumArtists) return <div>Chargement...</div>
+    }, [detailArtists ,albumArtists])
+    const handleRedirection = (name)=>{
+        history.push('/detail_album/' + name)
+    }
+    if(!detailArtists || !albumArtists) return <div>Chargement...</div>
     return (
-        <>
-            <div className='Detail_content'>
-                <div className='detail_artist'>
-                    <div className='name_artists'>
-                        <p className='nameArtists'>ARAFAT</p>
+        <div className='Detail_artists_container'>
+            {
+                detailArtists.map((detailArtist, index) => {
+                    return <div key={index} className='content-detail-artists'>
+                        <div className='infos-artists'>
+                            <div className='detail-artists'>
+                                <div className='img-artist'>
+                                    <img src={detailArtist.photo} />
+                                </div>
+                                <div className='infos-artist'>
+                                <p className='artist'>Artiste</p>
+                                    <p className='name_artist'>{detailArtist.name}</p>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                </div>
-            </div>
+                })
+            }
             <div className='artists_album-container'>
-                <p>Sortie populaire</p>
+                <div className='all-albums-artists'>
+                    <p>Albums</p>
+                </div>
                 <div className='artists-album-content'>
                     {
                         albumArtists.map((albumArtist, index) => {
-                            return <div key={index} className='artists-album'>
+                            return <div key={index} onClick={() => handleRedirection(albumArtist.name)} className='artists-album'>
                             <div className="img-album-artists">
                                 <img className="img-album-a" src={albumArtist.cover}/>
                             </div>
                             <div>
                                 <p>{albumArtist.name}</p>
-                                {/* <p>okdd;,,nsdc;s,dcnsdv</p> */}
                             </div>
                         </div>
                         })
                     }
                 </div>
             </div>
-        </>
+        </div>
     )
 }
 
